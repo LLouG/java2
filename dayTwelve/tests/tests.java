08badb26456eaea02542d7493b2ebdee3f0d13ef
Loug <lucianoguasque@gmail.com>
2025-04-07 23:49:11 -0300

    ...

--- dayTwelve/tests/tests.java
+++ dayTwelve/tests/tests.java
@@ -2,6 +2,32 @@ package dayTwelve.tests;
 
 public class tests {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Programmer person = new Programmer();
+        person.name = "Loug";
+        person.age = 32;
+        person.language = "Java";
+
+        person.showProgrammer();
+        person.showPerson();
+    }
+}
+
+
+class Person {
+    String name;
+    int age;
+
+    public void showPerson()
+    {
+        System.out.println(name + " is " + age + " years old.");
+    }
+}
+
+class Programmer extends Person {
+    String language;
+
+    public void showProgrammer()
+    {
+        System.out.println(name + " codes in " + language);
     }
 }
