69ff7f8c0d5ccdf7c9c82441bad46d30ff638104
Loug <lucianoguasque@gmail.com>
2025-04-10 01:29:17 -0300

    Today I learned more about Scanner

--- dayThirteen/Notes.java
+++ dayThirteen/Notes.java
@@ -1,7 +1,35 @@
 package dayThirteen;
+import java.util.Scanner;
 
 public class Notes {
     public static void main(String[] args) {
-        System.out.println("Reading docs...");
+        MultiLines myStr = new MultiLines();
+        myStr.scanStr();
+        myStr.printWord();
+    }
+}
+
+class MultiLines extends ScanStr {
+    public void printWord()
+    {
+        for (int i=0; i < message.length(); i++) 
+        {
+            for (int j=0; j < i; j++)
+            {
+                System.out.print(message.charAt(j));                
+            }
+            System.out.println(message.charAt(i));
+        }
+    }    
+}
+
+class ScanStr {
+    Scanner scanner = new Scanner(System.in);
+    public String message;
+
+    public void scanStr() 
+    {
+        System.out.println("Type a word: ");
+        message = scanner.nextLine();
     }
 }
