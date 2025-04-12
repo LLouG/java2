32590d3cda6509a2f555d08e4b45cf4fa6e65b7c
Loug <lucianoguasque@gmail.com>
2025-04-12 05:35:18 -0300

    :)

--- dayFifteen/Conditionals.java
+++ dayFifteen/Conditionals.java
@@ -1,8 +1,25 @@
 package dayFifteen;
 
+import java.util.Scanner;
+// alt + shift + F fix some of the indentation problems
+
 
 public class Conditionals {
     public static void main(String[] args) {
-        System.out.println("hi");
+        SpeedingTicket ticket = new SpeedingTicket();
+        ticket.speedingTicket(0);
+    }
+}
+
+class SpeedingTicket {
+    Scanner scanner = new Scanner(System.in);
+
+    public void speedingTicket(int speed) {
+        System.out.println("Give speed:");
+        speed = Integer.parseInt(scanner.nextLine());
+
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
