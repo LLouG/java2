89a436059bb6de707f00257624f42447c1e78eab
Loug <lucianoguasque@gmail.com>
2025-04-13 21:24:03 -0300

    Short but productive session today

--- daySixteen/Reader.java
+++ daySixteen/Reader.java
@@ -1,14 +1,59 @@
 package daySixteen;
 
+// TODO: percentageOfPositives = 100.0 * numberOfPositives / (numberOfPositives + numberOfNegatives
+
 import java.util.Scanner;
 
 public class Reader {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        /* Loops loops = new Loops();
+        loops.askNumbers(); */
 
-        String text = reader.nextLine();
+        ContinueLoop cloop = new ContinueLoop();
+        cloop.continueLoop();
+    }
+}
+
+
+class Loops {
+    Scanner reader = new Scanner(System.in);
+    int numbersRead;
+    int sum;
+
+    public void askNumbers() {
+        while (true) {
+            if (numbersRead == 5) {
+                break;
+            }
+
+            System.out.println("Input number:");
+            sum += Integer.valueOf(reader.nextLine());
+            numbersRead++;
+        }
+        System.out.println("The sum of the numbers is " + sum);
+    }
+    /* String text = reader.nextLine();
         int num = Integer.valueOf(reader.nextLine());
         double numberWithDecimals = Double.valueOf(reader.nextLine());
-        boolean trueOrFalse = Boolean.valueOf(reader.nextLine());
+        boolean trueOrFalse = Boolean.valueOf(reader.nextLine()); */
+}
+
+class ContinueLoop extends Loops {
+    public void continueLoop() {
+        while (true) {
+            System.out.println("Insert positive a number (0 to quit)");
+            int number = Integer.valueOf(reader.nextLine());
+
+            if (number < 0) {
+                System.out.println("Unfit number! Try again.");
+                continue;
+            }
+            
+            if (number == 0) {
+                break;
+            }
+            
+                System.out.println("The square of " + number + " is " + number * number);
+        }
     }
 }
