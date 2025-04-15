0470d7c8dea9370cd678fc75026c6819fb1df8f9
Loug <lucianoguasque@gmail.com>
2025-04-14 23:53:49 -0300

    having some more fun with loops

--- daySeventeen/MoreLoops.java
+++ daySeventeen/MoreLoops.java
@@ -1,9 +1,40 @@
 package daySeventeen;
+import java.util.Scanner;
 
-// TODO: percentageOfPositives = 100.0 * numberOfPositives / (numberOfPositives + numberOfNegatives
 
 public class MoreLoops {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        PositivesAndNegatives myNum = new PositivesAndNegatives();
+        myNum.askForNumbers();
     }    
+}
+
+class PositivesAndNegatives {
+    Scanner scanner = new Scanner(System.in);
+
+    int numberOfPositives;
+    int numberOfNegatives;
+    double percentageOfPositives;
+
+    public void askForNumbers() {
+            while (true) {            
+            int total = 0;
+            System.out.println("Enter a number (0 to quit)");
+
+            if (Integer.valueOf(scanner.nextLine()) < 0) {
+                numberOfNegatives++;
+                total++;
+            } else if (Integer.valueOf(scanner.nextLine()) > 0) {
+                numberOfPositives++;
+                total++;
+                // numberOfPositives++ only works after typing 2 positive numbers in a row, WHY?
+            } else {
+                percentageOfPositives = 100.0 * numberOfPositives / (numberOfPositives + numberOfNegatives);
+                System.out.println(percentageOfPositives + "% of the numbers were positive.");
+                break;
+            }
+
+            System.out.println("There are " + numberOfPositives + " positive numbers and " + numberOfNegatives + " negative numbers.");
+        }
+    }
 }
