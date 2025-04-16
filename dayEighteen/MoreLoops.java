abe55ec28e42a3e3621beeda6f39bf659dbf8fec
Loug <lucianoguasque@gmail.com>
2025-04-15 23:07:18 -0300

    added dayEighteen

---
+++ dayEighteen/MoreLoops.java
@@ -0,0 +1,7 @@
+package dayEighteen;
+
+public class MoreLoops {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- daySeventeen/CalcPercentage.java
+++ daySeventeen/CalcPercentage.java
@@ -1,12 +1,12 @@
 package daySeventeen;
+
 import java.util.Scanner;
 
-
-public class MoreLoops {
+public class CalcPercentage {
     public static void main(String[] args) {
         PositivesAndNegatives myNum = new PositivesAndNegatives();
         myNum.askForNumbers();
-    }    
+    }
 }
 
 class PositivesAndNegatives {
@@ -17,22 +17,21 @@ class PositivesAndNegatives {
     double percentageOfPositives;
 
     public void askForNumbers() {
-            while (true) {
+        while (true) {
             System.out.println("Enter a number (0 to quit)");
             int currentInput = Integer.valueOf(scanner.nextLine());
 
-            if (currentInput <= -1) 
-            {
+            if (currentInput <= -1) {
                 numberOfNegatives++;
-            }else if (currentInput >= 1) 
-            {
+            } else if (currentInput >= 1) {
                 numberOfPositives++;
             } else {
                 percentageOfPositives = 100.0 * numberOfPositives / (numberOfPositives + numberOfNegatives);
                 System.out.println(percentageOfPositives + "% of the numbers were positive.");
                 break;
             }
-            System.out.println("Currently there are " + numberOfPositives + " positive numbers and " + numberOfNegatives + " negative numbers");
+            System.out.println("Currently there are " + numberOfPositives + " positive numbers and " + numberOfNegatives
+                    + " negative numbers");
         }
     }
 }
