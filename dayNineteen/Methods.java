6e3071ce1f6af47429ee4afea7d85230067d4f57
Loug <lucianoguasque@gmail.com>
2025-04-19 00:00:02 -0300

    ...

--- dayNineteen/Methods.java
+++ dayNineteen/Methods.java
@@ -7,16 +7,12 @@ public class Methods {
         // printUntilNumber(5);
         // division(3, 5);
         // divisibleByThreeInRange();
-
-        System.out.println(alwaysReturn(7, 4));
+        // System.out.println(alwaysReturn(7, 4));
+        /* double number = 4 * alwaysReturnTen() + (alwaysReturnTen() / 2) - 8;
+        System.out.println("The result of the calculation is " + number); */
+        // beginningToMiddle(5,10);
     }
     
-    public static int alwaysReturn(int n1, int n2) {
-        int result = n1 + n2;
-
-        return result;
-    }
-
     public static void greet() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many times? ");
@@ -65,4 +61,39 @@ public class Methods {
         }
     }
 
+    public static int alwaysReturn(int n1, int n2) {
+        int result = n1 + n2;
+        
+        return result;
+    }
+    
+    public static int alwaysReturnTen() {
+        return 10;
+    }
+
+    public static void beginningToMiddle(int start, int end) {
+        int middle = (start + end) / 2;
+        while (start < middle) {
+            System.out.println("step");
+            start++;
+        }
+    }
+
+    public static void multiplicationTable(int max) {
+        int number = 1;
+
+        while (number < max) {
+            printMultiplicationTableRow(number, max);
+            number++;
+        }
+    }
+
+    public static void printMultiplicationTableRow(int number, int coefficient) {
+        int printable = number;
+        while (printable <= number * coefficient) {
+            System.out.print(" " + printable);
+            printable += number;
+        }
+        System.out.println("");
+    }
 }
